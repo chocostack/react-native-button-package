00e4d31bf2a64178e5dddfaf43e3eadf971e2451
DESKTOP-U7TEIJ5\Salim <salim.assad.sanchez@gmail.com>
2022-09-09 13:46:08 -0500

    no message

--- ButtonPackage.js
+++ ButtonPackage.js
@@ -15,37 +15,37 @@ export default class DefaultButton extends Component {
         }
 
         return <TouchableWithoutFeedback onPress={() => {
-                if (!this.props.disabled)
-                    this.props.onPress();
-            }}>
-                <View style={[{
-                    borderColor: '#404040',
-                    borderWidth: 1,
-                    padding: 8,
-                    borderRadius: 8,
-                    backgroundColor: 'white',
-                    flexDirection: 'row',
-                    ...disabledStyle
-                }, this.props.style]}>
-                    {this.props.loading ?
-                        <ActivityIndicator size={18} color={'#404040'} />
-                        :
-                        null
-                    }
-                    {this.props.icon && !this.props.loading ?
-                        this.props.icon
-                        :
-                        null}
-                    {this.props.icon && this.props.text ?
-                        <View style={{ width: 5 }}>
-                        </View>
-                        :
-                        null}
-                    <Text style={{ fontSize: 15, color: '#404040', ...this.props.textStyle }}>
-                        {this.props.text}
-                    </Text>
-                </View>
-            </TouchableWithoutFeedback>;
+            if (!this.props.disabled)
+                this.props.onPress();
+        }}>
+            <View style={[{
+                borderColor: '#404040',
+                borderWidth: 1,
+                padding: 8,
+                borderRadius: 8,
+                backgroundColor: 'white',
+                flexDirection: 'row',
+                ...disabledStyle
+            }, this.props.style]}>
+                {this.props.loading ?
+                    <ActivityIndicator size={18} color={'#404040'} />
+                    :
+                    null
+                }
+                {this.props.icon && !this.props.loading ?
+                    this.props.icon
+                    :
+                    null}
+                {this.props.icon && this.props.text ?
+                    <View style={{ width: 5 }}>
+                    </View>
+                    :
+                    null}
+                <Text style={{ fontSize: 15, color: '#404040', ...this.props.textStyle }}>
+                    {this.props.text}
+                </Text>
+            </View>
+        </TouchableWithoutFeedback>;
     }
 
 }
@@ -65,30 +65,35 @@ export class DarkButton extends Component {
 
 
         return <TouchableWithoutFeedback onPress={() => {
-                if (!this.props.disabled)
-                    this.props.onPress();
-            }}>
-                <View style={[{
-                    backgroundColor: '#404040',
-                    padding: 8,
-                    borderRadius: 8,
-                    flexDirection: 'row',
-                    ...disabledStyle
-                }, this.props.style]}>
-                    {this.props.icon ?
-                        this.props.icon
-                        :
-                        null}
-                    {this.props.icon && this.props.text ?
-                        <View style={{ width: 5 }}>
-                        </View>
-                        :
-                        null}
-                    <Text style={{ color: 'white', fontSize: 15, ...this.props.textStyle }}>
-                        {this.props.text}
-                    </Text>
-                </View>
-            </TouchableWithoutFeedback>;
+            if (!this.props.disabled)
+                this.props.onPress();
+        }}>
+            <View style={[{
+                backgroundColor: '#404040',
+                padding: 8,
+                borderRadius: 8,
+                flexDirection: 'row',
+                ...disabledStyle
+            }, this.props.style]}>
+                {this.props.loading ?
+                    <ActivityIndicator size={18} color={'white'} />
+                    :
+                    null
+                }
+                {this.props.icon ?
+                    this.props.icon
+                    :
+                    null}
+                {this.props.icon && this.props.text ?
+                    <View style={{ width: 5 }}>
+                    </View>
+                    :
+                    null}
+                <Text style={{ color: 'white', fontSize: 15, ...this.props.textStyle }}>
+                    {this.props.text}
+                </Text>
+            </View>
+        </TouchableWithoutFeedback>;
     }
 
 }
